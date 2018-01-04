e5bcd73d0393a1c37ea5af7d9639efad297e48f8
shurikkuzmin <shurikkuzmin@users.noreply.github.com>
2018-01-03 23:09:13 -0500

    Adding new data.

--- googlemaps/simple/fit.py
+++ googlemaps/simple/fit.py
@@ -10,7 +10,7 @@ class Fit(object):
     def __init__(self):
         print(os.getcwd())
         csv = pandas.read_excel("simple/data/table_clean.xlsx")
-        model = Pipeline([('poly', PolynomialFeatures(degree = 1)), ('linear', linear_model.Ridge(normalize = True, alpha = 1.0))])
+        model = Pipeline([('poly', PolynomialFeatures(degree = 3)), ('linear', linear_model.Ridge(normalize = True, alpha = 1.0))])
         model.fit(csv[["Salary","Years", "Position"]].values, csv["Delay"].values)
         self.model = model
         self.csv = csv
